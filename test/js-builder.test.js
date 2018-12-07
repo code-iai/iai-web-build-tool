17c8cea6975640d494c747904ba90035d71f30cd
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-12-07 14:10:17 +0100

    Removed redundant code by compressing repetitive test steps into one function

--- test/js-builder.test.js
+++ test/js-builder.test.js
@@ -27,42 +27,15 @@ test('Build a test js file', async () => {
     expect.assertions(7);
 
     await testJsBuilderIsSuccessful({useUglify: false, useBabel: false});
-
-    expect(fs.existsSync(RESULT_FILE_PATH)).toBe(true);
-
-    const testModule = require(`./temp/${OUTPUT_FILE}`);
-
-    let a = 10;
-    let b = 5;
-
-    expect(testModule.add(a, b)).toBe(a + b);
-    expect(testModule.subtract(a, b)).toBe(a - b);
-    expect(testModule.multiply(a, b)).toBe(a * b);
-    expect(testModule.divide(a, b)).toBe(a / b);
-    expect(testModule.helloWorld()).toBe('Hello World!');
+    testBuiltJsModule();
 });
 
 test('Build a babeled test js file', async () => {
     expect.assertions(8);
 
-
     await testJsBuilderIsSuccessful({useUglify: false, useBabel: true});
 
-    expect(fs.existsSync(RESULT_FILE_PATH)).toBe(true);
-
-    const testModule = require(`./temp/${OUTPUT_FILE}`);
-
-    let a = 10;
-    let b = 5;
-
-    expect(testModule.add(a, b)).toBe(a + b);
-    expect(testModule.subtract(a, b)).toBe(a - b);
-    expect(testModule.multiply(a, b)).toBe(a * b);
-    expect(testModule.divide(a, b)).toBe(a / b);
-    expect(testModule.helloWorld()).toBe('Hello World!');
-
-    let testStr = fs.readFileSync(RESULT_FILE_PATH, 'utf8');
-
+    const testStr = testBuildJsModuleAndReadIt();
     expect(testStr).toEqual(expect.stringContaining(' '));
 });
 
@@ -71,21 +44,8 @@ test('Build a uglified test js file', async () => {
 
     await testJsBuilderIsSuccessful({useUglify: true, useBabel: false});
 
-    expect(fs.existsSync(EXISTING_SOURCE_FILE_PATH)).toBe(true);
-
-    const testModule = require(`./temp/${OUTPUT_FILE}`);
-
-    let a = 10;
-    let b = 5;
-    expect(testModule.add(a, b)).toBe(a + b);
-    expect(testModule.subtract(a, b)).toBe(a - b);
-    expect(testModule.multiply(a, b)).toBe(a * b);
-    expect(testModule.divide(a, b)).toBe(a / b);
-    expect(testModule.helloWorld()).toBe('Hello World!');
-
-    let testStr = fs.readFileSync(RESULT_FILE_PATH, 'utf8');
-
-    let lineBreakCount = (testStr.match(/\n/g) || []).length;
+    const testStr = testBuildJsModuleAndReadIt();
+    const lineBreakCount = (testStr.match(/\n/g) || []).length;
     // Two linebreaks are always included
     // One for the sourcemap link and one as the final token
     expect(lineBreakCount).toBeLessThanOrEqual(2);
@@ -100,6 +60,24 @@ test('Throw ReferenceError when source does not exist.', async () => {
         beStandalone: true,
     })).rejects.toThrowError(ReferenceError);
 });
+
+function testBuildJsModuleAndReadIt(){
+    testBuiltJsModule();
+    return fs.readFileSync(RESULT_FILE_PATH, 'utf8');
+}
+function testBuiltJsModule(){
+    expect(fs.existsSync(EXISTING_SOURCE_FILE_PATH)).toBe(true);
+
+    const testModule = require(`./temp/${OUTPUT_FILE}`);
+
+    let a = 10;
+    let b = 5;
+    expect(testModule.add(a, b)).toBe(a + b);
+    expect(testModule.subtract(a, b)).toBe(a - b);
+    expect(testModule.multiply(a, b)).toBe(a * b);
+    expect(testModule.divide(a, b)).toBe(a / b);
+    expect(testModule.helloWorld()).toBe('Hello World!');
+}
 
 async function testJsBuilderIsSuccessful({ useUglify, useBabel }){
     // All js builds have to be standalone, because otherwise node js cannot execute them
