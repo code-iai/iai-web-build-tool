b1ae9228880d1dc1e6f4b55f424296d0eeb5686c
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-12-07 14:26:45 +0100

    removed not required string in js builder test

--- test/js-builder.test.js
+++ test/js-builder.test.js
@@ -12,7 +12,7 @@ const NOT_EXISTING_SRC_FILE = 'nonexistent.js';
 
 const NOT_EXISTING_SOURCE_FILE_PATH = path.join(TEST_DIR, NOT_EXISTING_SRC_FILE);
 const EXISTING_SOURCE_FILE_PATH = path.join(TEST_DIR, EXISTING_SRC_FILE);
-const RESULT_FILE_PATH = path.join('.',testBase.tempDir, OUTPUT_FILE);
+const RESULT_FILE_PATH = path.join(testBase.tempDir, OUTPUT_FILE);
 
 /*
 Since we are testing async code, it is required to count all assertions to make sure,
