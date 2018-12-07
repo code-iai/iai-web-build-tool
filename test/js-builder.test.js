3e538d8c6417d0dc476c54dccedf1068fe052805
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-12-07 13:51:13 +0100

    File paths are represented as constants in test js builder

--- test/js-builder.test.js
+++ test/js-builder.test.js
@@ -4,6 +4,18 @@ const path = require('path');
 const jsBuilder = require('../src/js-builder');
 const tB = require('./test-base');
 
+const TEST_DIR = './test/src/js';
+const EXISTING_SRC_FILE = 'base.js';
+const OUTPUT_FILE = 'main.js';
+const OUTPUT_FILE_MAP = 'main.js.map';
+
+const NOT_EXISTING_SRC_FILE = 'nonexistent.js';
+
+const NOT_EXISTING_SOURCE_FILE_PATH = path.join(TEST_DIR, NOT_EXISTING_SRC_FILE);
+const EXISTING_SOURCE_FILE_PATH = path.join(TEST_DIR, EXISTING_SRC_FILE);
+const RESULT_FILE_PATH = path.join('.',tB.tempDir, OUTPUT_FILE);
+const RESULT_FILE_MAP_PATH = path.join('.',tB.tempDir, OUTPUT_FILE_MAP);
+
 beforeEach(() => {
     tB.createTempDir();
 });
@@ -13,27 +25,19 @@ afterEach(() => {
 });
 
 test('Build a test js file', async () => {
-    const testDir = './test/src/js';
-    const srcFile = 'base.js';
-    const outputFile = 'main.js';
-    const outputFileMap = 'main.js.map';
-
-    const sourceFilePath = path.join(testDir, srcFile);
-    const resultFilePath = path.join(tB.tempDir, outputFile);
-
     expect.assertions(8);
 
     // All js builds have to be standalone, because otherwise node js cannot execute them
     await expect(jsBuilder.build({
-        sourceFilePath,
-        resultFilePath,
+        sourceFilePath: EXISTING_SOURCE_FILE_PATH,
+        resultFilePath: RESULT_FILE_PATH,
         beStandalone: true,
     })).resolves.toBeTruthy();
 
-    expect(fs.existsSync(resultFilePath)).toBe(true);
-    expect(fs.existsSync(tB.tempDir, outputFileMap)).toBe(true);
+    expect(fs.existsSync(RESULT_FILE_PATH)).toBe(true);
+    expect(fs.existsSync(RESULT_FILE_MAP_PATH)).toBe(true);
 
-    const testModule = require(`./temp/${outputFile}`);
+    const testModule = require(`./temp/${OUTPUT_FILE}`);
 
     let a = 10;
     let b = 5;
@@ -45,35 +49,28 @@ test('Build a test js file', async () => {
 });
 
 test('Build a babeled test js file', async () => {
-    const testDir = './test/src/js';
-    const srcFile = 'base.js';
-    const outputFile = 'main.js';
-    const outputFileMap = 'main.js.map';
-
-    const sourceFilePath = path.join(testDir, srcFile);
-    const resultFilePath = path.join(tB.tempDir, outputFile);
-
     expect.assertions(9);
 
     // All js builds have to be standalone, because otherwise node js cannot execute them
     await expect(jsBuilder.build({
-        sourceFilePath,
-        resultFilePath,
+        sourceFilePath: EXISTING_SOURCE_FILE_PATH,
+        resultFilePath: RESULT_FILE_PATH,
         useBabel: true,
         beStandalone: true,
     })).resolves.toBeTruthy();
 
-    expect(fs.existsSync(resultFilePath)).toBe(true);
-    expect(fs.existsSync(tB.tempDir, outputFileMap)).toBe(true);
+    expect(fs.existsSync(RESULT_FILE_PATH)).toBe(true);
+    expect(fs.existsSync(RESULT_FILE_MAP_PATH)).toBe(true);
 
-    let testStr = fs.readFileSync(resultFilePath, 'utf8');
+    let testStr = fs.readFileSync(RESULT_FILE_PATH, 'utf8');
 
     expect(testStr).toEqual(expect.stringContaining(' '));
 
-    const testModule = require(`./temp/${outputFile}`);
+    const testModule = require(`./temp/${OUTPUT_FILE}`);
 
     let a = 10;
     let b = 5;
+
     expect(testModule.add(a, b)).toBe(a + b);
     expect(testModule.subtract(a, b)).toBe(a - b);
     expect(testModule.multiply(a, b)).toBe(a * b);
@@ -82,35 +79,27 @@ test('Build a babeled test js file', async () => {
 });
 
 test('Build a uglified test js file', async () => {
-    const testDir = './test/src/js';
-    const srcFile = 'base.js';
-    const outputFile = 'main.js';
-    const outputFileMap = 'main.js.map';
-
-    const sourceFilePath = path.join(testDir, srcFile);
-    const resultFilePath = path.join(tB.tempDir, outputFile);
-
     expect.assertions(9);
 
     // All js builds have to be standalone, because otherwise node js cannot execute them
     await expect(jsBuilder.build({
-        sourceFilePath,
-        resultFilePath,
+        sourceFilePath: EXISTING_SOURCE_FILE_PATH,
+        resultFilePath: RESULT_FILE_PATH,
         useUglify: true,
         beStandalone: true,
     })).resolves.toBeTruthy();
 
-    expect(fs.existsSync(sourceFilePath)).toBe(true);
-    expect(fs.existsSync(tB.tempDir, outputFileMap)).toBe(true);
+    expect(fs.existsSync(EXISTING_SOURCE_FILE_PATH)).toBe(true);
+    expect(fs.existsSync(RESULT_FILE_MAP_PATH)).toBe(true);
 
-    let testStr = fs.readFileSync(resultFilePath, 'utf8');
+    let testStr = fs.readFileSync(RESULT_FILE_PATH, 'utf8');
 
     let lineBreakCount = (testStr.match(/\n/g) || []).length;
     // Two linebreaks are always included
     // One for the sourcemap link and one as the final token
     expect(lineBreakCount).toBeLessThanOrEqual(2);
 
-    const testModule = require(`./temp/${outputFile}`);
+    const testModule = require(`./temp/${OUTPUT_FILE}`);
 
     let a = 10;
     let b = 5;
@@ -122,18 +111,10 @@ test('Build a uglified test js file', async () => {
 });
 
 test('Throw ReferenceError when source does not exist.', async () => {
-    const testDir = './test/src/js';
-    const outputFile = 'main.js';
-    const nonexistentFile = 'nonexistent.js';
-
-    const sourceFilePath = path.join(testDir, nonexistentFile);
-    const resultFilePath = path.join(tB.tempDir, outputFile);
-
     expect.assertions(1);
-
-    expect(jsBuilder.build({
-        sourceFilePath,
-        resultFilePath,
+    await expect(jsBuilder.build({
+        sourceFilePath: NOT_EXISTING_SOURCE_FILE_PATH,
+        resultFilePath: RESULT_FILE_PATH,
         beStandalone: true,
     })).rejects.toThrowError(ReferenceError);
 });
